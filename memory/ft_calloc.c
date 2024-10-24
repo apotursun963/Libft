290bf48f2c5d0492290cb94e7e430ca599848fa9
Abdullah Tursun <124070221+apotursun963@users.noreply.github.com>
2024-10-24 11:25:55 +0300

    Add files via upload

---
+++ memory/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atursun <atursun@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 14:35:19 by atursun           #+#    #+#             */
+/*   Updated: 2024/10/24 10:44:52 by atursun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t num_blocks, size_t block_size)
+{
+	char	*str;
+
+	if (!(str = (char *)malloc(num_blocks * block_size)))
+		return (NULL);
+	ft_bzero(str, num_blocks * block_size);
+	return (str);
+}
