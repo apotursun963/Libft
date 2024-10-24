ba97b2fd1dc2f902cf5cde567b8e4b5eb3174476
Abdullah Tursun <124070221+apotursun963@users.noreply.github.com>
2024-10-24 11:44:29 +0300

    Update ft_memset.c

--- memory/ft_memset.c
+++ memory/ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
 void	*ft_memset(void *str, int chr, size_t len)
 {
