17a165a6c27b3d13db8718066cbc186f5617896c
Abdullah Tursun <124070221+apotursun963@users.noreply.github.com>
2024-10-24 11:43:42 +0300

    Update ft_calloc.c

--- memory/ft_calloc.c
+++ memory/ft_calloc.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
 void	*ft_calloc(size_t num_blocks, size_t block_size)
 {
