05695c2491a24ad9bfb5d707ed6bb2bf2e452e09
abdullah tursun <124070221+apotursun963@users.noreply.github.com>
2024-11-06 19:59:45 +0300

    Update ft_split.c

--- strings/ft_split.c
+++ strings/ft_split.c
@@ -81,7 +81,7 @@ char	**ft_split(char const *str, char c)
 {
 	char	**arr;
 
-	if (!(str) || !(arr = (char **)malloc((ft_len_of_word(str, c) + 1) * sizeof(char *)))
+	if (!(str) || !(arr = (char **)malloc((ft_len_of_word(str, c) + 1) * sizeof(char *))))
 		return (NULL);
 	arr = ft_split_string(arr, str, c);
 	return (arr);
